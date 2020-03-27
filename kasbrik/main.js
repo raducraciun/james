d094f353bd9b2f09215939dcabce87d53f14d0ac
raducraciun <craciunradu78150@gmail.com>
2020-03-27 15:17:56 +0100

    Boucle de jeu vide ajoutée

---
+++ kasbrik/main.js
@@ -0,0 +1,9 @@
+var ctx = document.getElementById("canvas").getContext("2d") // Contexte 2D
+
+function draw() { // Boucle de jeu
+  
+
+  requestAnimationFrame(draw)
+}
+
+requestAnimationFrame(draw)
