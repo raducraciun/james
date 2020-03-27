2904aac91496c2be4e0bdc2760da3a3755648d5c
raducraciun <craciunradu78150@gmail.com>
2020-03-27 16:08:12 +0100

    Balle statique ajoutée

--- kasbrik/main.js
+++ kasbrik/main.js
@@ -1,9 +1,24 @@
-var ctx = document.getElementById("canvas").getContext("2d") // Contexte 2D
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d"); // Contexte 2D
 
-function draw() { // Boucle de jeu
-  
+var ball = {
+  radius: 10,
+  x: canvas.width/2,
+  y: canvas.height - 30
+};
 
-  requestAnimationFrame(draw)
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI*2);
+  ctx.fillStyle = "blue";
+  ctx.fill();
+  ctx.closePath();
 }
 
-requestAnimationFrame(draw)
+function draw() { // Boucle de jeu
+  drawBall();
+
+  requestAnimationFrame(draw);
+}
+
+requestAnimationFrame(draw);
