3d04d0162ee312435563d8b503f35145d880a91d
raducraciun <craciunradu78150@gmail.com>
2020-03-29 11:03:12 +0200

    Cellules dessinées

--- life/main.js
+++ life/main.js
@@ -12,8 +12,33 @@ for (var i = 0; i < nbRows; i++) {
   }
 }
 
+function drawCells() {
+  for (var i = 0; i < nbRows; i++) {
+    for (var j = 0; j < nbCols; j++) {
+      var cell = cells[i][j];
+      var cellPad = 2;
+      var cellW = canvas.width/nbCols - cellPad, cellH = canvas.width/nbCols - cellPad;
+      if (i == j) {
+        cell.isAlive = true;
+      }
+      var x = j*(cellW + cellPad) + 1;
+      var y = i*(cellH + cellPad) + 1;
+      ctx.beginPath();
+      ctx.rect(x, y, cellW, cellH);
+      if (cell.isAlive == true) {
+        ctx.fillStyle = "#009900"
+      }
+      else {
+        ctx.fillStyle = "#aaa";
+      }
+      ctx.fill();
+      ctx.closePath();
+    }
+  }
+}
 
 function gameLoop() {
+  drawCells();
 
 }
 
