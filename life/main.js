76b1393e40f047f45cb4ec35cf2dff38408870c4
raducraciun <craciunradu78150@gmail.com>
2020-03-29 12:07:55 +0200

    Dessin au clic supporté
    Cliquer dans la grille change l'état de la cellule cliquée.

--- life/main.js
+++ life/main.js
@@ -17,10 +17,8 @@ function drawCells() {
     for (var j = 0; j < nbCols; j++) {
       var cell = cells[i][j];
       var cellPad = 2;
-      var cellW = canvas.width/nbCols - cellPad, cellH = canvas.width/nbCols - cellPad;
-      if (i == j) {
-        cell.isAlive = true;
-      }
+      var cellW = canvas.width/nbCols - cellPad,
+          cellH = canvas.width/nbCols - cellPad;
       var x = j*(cellW + cellPad) + 1;
       var y = i*(cellH + cellPad) + 1;
       ctx.beginPath();
@@ -37,9 +35,38 @@ function drawCells() {
   }
 }
 
+var clickedButton = {
+  main: false
+};
+
+var mouseRelativePosition = { // Position relative à l'élément cliqué
+  x: 0,
+  y: 0
+};
+
+function mouseClickHandler(event) {
+  if (event.button == 0) { // Clic principal (souvent gauche)
+    clickedButton.main = true;
+    mouseRelativePosition.x = event.offsetX;
+    mouseRelativePosition.y = event.offsetY;
+  }
+}
+
+canvas.addEventListener("click", mouseClickHandler, false);
+
 function gameLoop() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height); // Efface le canvas
   drawCells();
 
+  if (clickedButton.main) {
+    var r = Math.floor(mouseRelativePosition.y*nbRows/canvas.height),
+        c = Math.floor(mouseRelativePosition.x*nbCols/canvas.width);
+
+    cells[r][c].isAlive = !cells[r][c].isAlive;
+  }
+  clickedButton.main = false;
+
+  requestAnimationFrame(gameLoop);
 }
 
 gameLoop();
