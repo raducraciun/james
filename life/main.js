a28b71be9a769355aeebc9b1967496cddf456514
raducraciun <craciunradu78150@gmail.com>
2020-03-29 11:02:54 +0200

    Tableau de cellules ajouté

--- life/main.js
+++ life/main.js
@@ -1,10 +1,19 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d"); // Contexte 2D
 
+var cells = [];
+var nbCols = canvas.width/16, // 40
+nbRows = canvas.height/16; // 30
+
+for (var i = 0; i < nbRows; i++) {
+  cells[i] = [];
+  for (var j = 0; j < nbCols; j++) {
+    cells[i][j] = {isAlive: false};
+  }
+}
 
 
 function gameLoop() {
-
 
 }
 
